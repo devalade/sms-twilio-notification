a6665c78770cea9c321b5a78157e3cf1ce6f562a
devalade <74435372+devalade@users.noreply.github.com>
2021-06-09 13:27:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Twilio SMS API Test
+
+[Twilio SMS API Overview](https://www.twilio.com/docs/sms/api)
+# twilio-sms-notification
+# sms-twilio-notification
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser')
+
+
+//set template engine
+app.set('view engine', 'ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/sendSMS", (req, res) => {
+  res.render("sendSms.ejs");
+})
+
+app.post("/sendSMS", function (req, res) {
+  const {number, content} = req.body;
+  console.log(number, content);
+  res.redirect("/sendSMS");
+
+  const accountSid = 'ACb0b0b19903b71165a978c48b7947c1ca'; 
+  const authToken = 'd021c6fd04b4e6bfc015352eff87349b'; 
+  const client = require('twilio')(accountSid, authToken); 
+ 
+  client.messages 
+      .create({ 
+         body: `${content}`,  
+         messagingServiceSid: 'MG68b9e9e5b2085235751b5df2cf9d8b3a',      
+         to: `${number}` 
+       }) 
+      .then(message => console.log(message.body)) 
+      .done();
+});
+
+app.listen(3000);
